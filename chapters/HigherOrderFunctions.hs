bf8b344d1ef81373946f306026e02766fe4576ff
Pierre-Marie Dartus <p.dartus@salesforce.com>
2016-11-26 11:33:04 +0100

    Higher order functions pt. 2

--- chapters/HigherOrderFunctions.hs
+++ chapters/HigherOrderFunctions.hs
@@ -7,8 +7,8 @@
 multThree :: (Num a) => a -> a -> a -> a
 multThree x y z = x * y * z
 
--- Notice here the compare parameter is not explecitly
--- defined in the function body
+-- Notice here the compare parameter is not explecitly defined in the function 
+-- body
 
 compareWithHundred :: (Num a, Ord a) => a -> Ordering
 compareWithHundred = compare 100
@@ -38,9 +38,9 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f x y = f y x
 
-map' :: (a -> b) -> [a] -> [b]
-map' _ [] = []
-map' f (x:xs) = f x : map' f xs
+-- map' :: (a -> b) -> [a] -> [b]
+-- map' _ [] = []
+-- map' f (x:xs) = f x : map' f xs
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
@@ -59,3 +59,43 @@ chain 1 = [1]
 chain n
   | even n  = n:chain (n `div` 2)
   | odd n   = n:chain (n*3 + 1)
+
+-- Lambdas
+-- =======
+
+numLongChains :: Int
+numLongChains = length ( filter (\xs -> length xs > 15) ( map chain [1..100]))
+
+-- Used with map
+-- map (\(a,b) -> a + b) [(1,2),(3,4)]
+
+-- Are equivalents
+-- AddThree = \x -> \y -> \z -> x + y + z
+-- AddThree x y z = x + y + z
+
+-- More readable way to define flip using lambda
+-- flip' :: (a, b, c) -> b -> a -> c
+-- flip' f = \x y -> f y x
+
+-- Fold
+-- ====
+
+sum' :: (Num a) => [a] -> a
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x = foldl (\acc y -> if x == y then True else acc) False
+
+-- Can also be done via foldl however it's less expensive to do it with foldr
+-- : operator is less expensive than ++
+map' :: ( a -> b ) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+-- Note: foldl is traversing the list in the left dirction from right to left
+-- This function can't be used on infinite list
+
+-- foldl1 and foldr1 use the first and last value respectivly as init value for 
+-- the accumulator
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)   
